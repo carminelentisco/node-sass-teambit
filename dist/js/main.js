6c0dddaa42f4c20c120cb430c012556a2ea1a507
Carmine Lentisco <carminelentisco@gmail.com>
2020-05-13 18:18:51 +0200

    JS

--- dist/js/main.js
+++ dist/js/main.js
@@ -7,7 +7,14 @@
 /*! unknown exports (runtime-defined) */
 /*! exports [maybe provided (runtime-defined)] [unused] */
 /*! runtime requirements:  */
-console.log('Sono stato inserito correttamente');
+console.log('Sono stato inserito correttamente'); // setup
+
+var menuList = $('.menu-list li');
+var subMenu = $('sub-menu');
+menuList.on('mouseenter', function () {
+  var menuListItem = $(this).menuList;
+  $('.sub-menu').show();
+});
 })();
 
 (() => {
