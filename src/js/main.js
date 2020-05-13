8d0b3cd81a48423e1f6c2609267c5cf27b671b4e
Carmine Lentisco <carminelentisco@gmail.com>
2020-05-13 18:18:33 +0200

    JS

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,10 @@
 console.log('Sono stato inserito correttamente');
+
+// setup
+
+var menuList = $('.menu-list li');
+var subMenu = $('sub-menu');
+menuList.on('mouseenter', function() {
+    var menuListItem = $(this).menuList;
+    $('.sub-menu').show();
+});
