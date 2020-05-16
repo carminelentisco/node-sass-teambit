bdc283842c2d79f88fe0f9dc9e4b1f4c8817df37
Carmine Lentisco <carminelentisco@gmail.com>
2020-05-16 04:04:29 +0200

    JS
     - Aggiunta interattività in versione desktop
     - Aggiunta interattività mobile
     - Aggiunto  controllo dimensione schermo

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,28 @@
 jQuery(document).ready(function($) {
 
     // VAR
-    
+    var listItem = $('.menu-list-descktop li');
+    var mobileMenuIcon = $('#mobile__menu-icon');
+    var menuListMobile = $('.menu-list-mobile');
 
+    listItem.mouseenter(function () { 
+        $(this).find('.sub-menu').fadeIn();
+    });
+
+    listItem.mouseleave(function () { 
+        $(this).find('.sub-menu').fadeOut();
+    });
+
+    mobileMenuIcon.on('click',function() {
+        menuListMobile.toggle();
+    });
+
+    setInterval( function() {
+        var size = window.innerWidth;
+        
+        if ( size > 768 ) {
+            menuListMobile.hide();
+        }  
+    }, 1);
+    
 }); ////////////////////////////////
